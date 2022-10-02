9ea0e47f75cab21390617175ab62a4f041042a3b
Plume <a.pristchepa@gmail.com>
2022-10-02 21:16:16 +0200

    feat: ui init

--- Assets/Scripts/UIDriver.cs
+++ Assets/Scripts/UIDriver.cs
@@ -8,10 +8,15 @@ public class UIDriver : MonoBehaviour
     private float _timeTillNextScan;
 
     public float TimeTillNextScan {get => _timeTillNextScan; set {_timeTillNextScan = value;}}
+    public PlayerController PlayerController;
     public TMP_Text timeTillNextScanNumberText;
+    public GameObject speedArrow;
 
     void Update()
     {
         timeTillNextScanNumberText.text = _timeTillNextScan.ToString("0.00");
+        // assing rotation to arrow depending on the velocity 
+        Debug.Log(PlayerController.Velocity.magnitude);
+        speedArrow.transform.rotation = Quaternion.Euler(0, 0, PlayerController.Velocity.magnitude * 500);
     }
 }
