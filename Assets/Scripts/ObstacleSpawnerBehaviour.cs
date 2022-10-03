fe570d73d7632aedd988e583dbb439305b1a4a2e
Xarius <jules.sab132@outlook.fr>
2022-10-04 00:09:06 +0200

    feat: obstacleSpawner

---
+++ Assets/Scripts/ObstacleSpawnerBehaviour.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawnerBehaviour : MonoBehaviour
+{
+    public GameObject[] obstacles;
+    public int spawnAmount;
+    int _obstaclesLength;
+    List<Vector2> _spawnPoints;
+
+    void Start()
+    {
+        _obstaclesLength = obstacles.Length;
+        _spawnPoints = new List<Vector2>();
+        SpawnObstacles(spawnAmount);
+    }
+
+
+
+    public void SpawnObstacles(int amount)
+    {
+        for (int i = 0; i <= amount; i++)
+        {
+            Vector2 spawnPoint;
+            int iterationCounter = 0;
+            do {
+                spawnPoint = (Vector2)transform.position + Random.insideUnitCircle.normalized * 2.5f + new Vector2(Random.Range(-0.75f, 0.75f),Random.Range(-0.75f, 0.75f));
+                iterationCounter++;
+            } while (_spawnPoints.FindAll(sp => Vector2.SqrMagnitude(spawnPoint - sp) < 4f).Count > 0 && iterationCounter < 20);
+            if (iterationCounter == 20)
+                Debug.LogWarning("Could not find a suitable position in 20 iterations.");
+
+            Instantiate(
+                obstacles[Random.Range(0, _obstaclesLength - 1)],
+                spawnPoint,
+                Quaternion.Euler(0f, 0f, 90f*(int)(Random.value*4f))
+            );
+            
+            _spawnPoints.Add(spawnPoint);
+        }
+    }  
+}
