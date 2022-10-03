6df26ea69c2e34d0e0d9cee7286cdd63013373a6
Xarius <jules.sab132@outlook.fr>
2022-10-03 21:53:24 +0200

    feat: pp main menu

---
+++ Assets/Scripts/PostProcessingInterface.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class PostProcessingInterface : MonoBehaviour
+{
+    VolumeProfile volumeProfile;
+    Vignette vignette;
+    void Start()
+    {
+        volumeProfile = GetComponent<Volume>()?.profile;
+        if(!volumeProfile) throw new System.NullReferenceException(nameof(UnityEngine.Rendering.VolumeProfile));
+    }     
+
+    void Update()
+    {    
+        if(!volumeProfile.TryGet(out vignette)) throw new System.NullReferenceException(nameof(vignette));
+        
+        vignette.intensity.Override(0.5f);
+    } 
+
+}
